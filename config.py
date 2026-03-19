f622bc0a33a45912c4466ce300ab931cc989b825
Bluntest64 <123kevindavidgomezposada@gmail.com>
2026-03-19 15:04:35 -0500

    Update MySQL configuration environment variables

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_secreta_concesionario_2024')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'concesionario_db')
+
+    MYSQL_HOST = os.environ.get('MYSQLHOST')
+    MYSQL_USER = os.environ.get('MYSQLUSER')
+    MYSQL_PASSWORD = os.environ.get('MYSQLPASSWORD')
+    MYSQL_DB = os.environ.get('MYSQL_DATABASE')
+    MYSQL_PORT = int(os.environ.get('MYSQLPORT', 3306))
+
     MYSQL_CURSORCLASS = 'DictCursor'
